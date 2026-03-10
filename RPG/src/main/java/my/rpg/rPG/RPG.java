b8366e03cf18baecb0529f53d8fdff7186307902
Jason <jasoncdrea@gmail.com>
2026-03-10 17:01:50 +0100

    [ADD] Minimum files

---
+++ RPG/src/main/java/my/rpg/rPG/RPG.java
@@ -0,0 +1,17 @@
+package my.rpg.rPG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
