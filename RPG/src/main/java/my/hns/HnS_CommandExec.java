259a442ee4c3cbf3b468570bb2c761334850bb81
Jason <jasoncdrea@gmail.com>
2026-03-13 13:34:16 +0100

    [ADD] join, hiders & seekers commands

--- RPG/src/main/java/my/hns/HnS_CommandExec.java
+++ RPG/src/main/java/my/hns/HnS_CommandExec.java
@@ -1,25 +1,63 @@
 package my.hns;
 
+import my.rpg.rPG.RPG;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Logger;
 
 public class HnS_CommandExec implements CommandExecutor {
 
+    private final Logger logger = JavaPlugin.getProvidingPlugin(HnS_CommandExec.class).getLogger();
+    private final HnSMain hnsmain = new HnSMain(logger);
+
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-
         if(command.getName().equalsIgnoreCase("squidGame")) {
             if (sender instanceof Player player) {
+                logger.info("command : squidGame launched");
                 player.sendMessage("SquidGame!");
                 return true;
             }
         }
-
-
-
+        if(command.getName().equalsIgnoreCase("join")) {
+            if(args[0].equalsIgnoreCase("hiders")) {
+                if (sender instanceof Player player) {
+                    player.sendMessage("Joining hiders!");
+                    return true;
+                }
+            }
+            if(args[0].equalsIgnoreCase("seekers")) {
+                if (sender instanceof Player player) {
+                    player.sendMessage("Joining seekers!");
+                    return true;
+                }
+            }
+        }
+        if(command.getName().equalsIgnoreCase("hiders")){
+            if(args[0].equalsIgnoreCase("list")) {
+                if (sender instanceof Player) {
+                    logger.info("command : hiders list launched");
+                    return true;
+                }
+            }
+        }
+        // LIST
+        if(command.getName().equalsIgnoreCase("seekers")) {
+            if(args[0].equalsIgnoreCase("list")) {
+                if (sender instanceof Player player) {
+                    logger.info("command : seekers list launched");
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
