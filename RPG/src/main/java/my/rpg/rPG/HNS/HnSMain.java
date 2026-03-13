3ab2ac49b110502134e207d9c6602cfcaa7a73b1
S-oul <S.soulslasher@gmail.com>
2026-03-13 11:45:02 +0100

    [ADD] Enabling File from start

--- RPG/src/main/java/my/rpg/rPG/HNS/HnSMain.java
+++ RPG/src/main/java/my/rpg/rPG/HNS/HnSMain.java
@@ -1,4 +1,23 @@
 package my.rpg.rPG.HNS;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+
 public class HnSMain {
+
+    private Logger _logger;
+
+    public HnSMain(Logger logger) {
+        _logger = logger;
+    }
+
+    public void onEnable() {
+        _logger.info("Hello From HnS");
+    }
+
+    public void onDisable() {
+
+    }
 }
--- RPG/src/main/java/my/rpg/rPG/RPG.java
+++ RPG/src/main/java/my/rpg/rPG/RPG.java
@@ -1,17 +1,21 @@
 package my.rpg.rPG;
 
+import my.rpg.rPG.HNS.HnSMain;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RPG extends JavaPlugin {
 
+    private HnSMain _HnSmain = new HnSMain(getLogger());
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Hello From RPG");
 
+        _HnSmain.onEnable();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        _HnSmain.onDisable();
     }
 }
