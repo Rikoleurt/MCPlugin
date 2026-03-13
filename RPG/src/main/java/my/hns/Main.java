11b253ae07fc7acff98bdafdc8d0292c13bff859
Jason <jasoncdrea@gmail.com>
2026-03-13 16:34:30 +0100

    [UPDATE] Updated item meta data & style

--- RPG/src/main/java/my/hns/Main.java
+++ RPG/src/main/java/my/hns/Main.java
@@ -1,6 +1,7 @@
 package my.hns;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -14,8 +15,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 
 public final class Main extends JavaPlugin implements Listener {
 
@@ -53,7 +53,7 @@ public final class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        System.out.println("Player joined: " + event.getPlayer().getName());
+
         event.joinMessage(Component.text("Welcome to the server : " + event.getPlayer().getName()));
 
         // Game instruction
@@ -61,17 +61,23 @@ public final class Main extends JavaPlugin implements Listener {
         player.sendMessage("** Please join a team with the /join <team> command ** ");
         player.sendMessage("** There must be at least one player in the seekers team and one player in the hiders team **");
 
+        // Lobby set up
+        Inventory playerInv = player.getInventory();
+        if(playerInv.getItem(0) == null) playerInv.addItem(new ItemStack(Material.DIAMOND));
+
         int playerNb = getServer().getOnlinePlayers().size();
         getLogger().info("Player number: " + playerNb);
 
         getServer().getPluginManager().registerEvents(this, this);
     }
-    //new ItemStack(Material.DIAMOND)
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Inventory inv = Bukkit.createInventory(null, 27, Component.text("Team selector"));
-//        inv.setItem(12, metaData("Seekers", Material.LAPIS_LAZULI));
-//        inv.setItem(14, metaData(new ItemStack(Material.EMERALD), "Hiders", Material.DIAMOND));
+        ItemStack sword = new ItemStack(Material.SOUL_LANTERN);
+        ItemStack pot = new ItemStack(Material.FLOWER_POT);
+        inv.setItem(12, metaData(sword, "Seekers"));
+        inv.setItem(14, metaData(pot, "Hiders"));
         if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.DIAMOND)) {
                 getLogger().info("Player interacted with: " + Material.DIAMOND);
@@ -82,19 +88,30 @@ public final class Main extends JavaPlugin implements Listener {
 
 
     private void LoadCommands() {
-
         HnS_CommandExec _commandExec = new HnS_CommandExec(this);
 
         Objects.requireNonNull(getCommand("squidGame")).setExecutor(_commandExec);
         Objects.requireNonNull(getCommand("StartGame")).setExecutor(_commandExec);
         Objects.requireNonNull(getCommand("join")).setExecutor(_commandExec);
         Objects.requireNonNull(getCommand("team")).setExecutor(_commandExec);
-
     }
 
-    private void metaData(ItemStack i, String name, Material material, String ... lore) {
+    private ItemStack metaData(ItemStack i, String name, String ... lore) {
         ItemMeta meta = i.getItemMeta();
-        meta.displayName(Component.text(name));
+        if(i.getType().equals(Material.SOUL_LANTERN)){
+            meta.displayName(Component.text(name, TextColor.color(0x2CE4FF)));
+            List<Component> lores = new ArrayList<>();
+            Collections.addAll(lores, Component.text("Join the seekers!", TextColor.color(0x2CE4FF)));
+            meta.lore(lores);
+        }
+        if(i.getType().equals(Material.FLOWER_POT)){
+            meta.displayName(Component.text(name, TextColor.color(0xFF964A)));
+            List<Component> lores = new ArrayList<>();
+            Collections.addAll(lores, Component.text("Join the hiders!", TextColor.color(0xFF964A)));
+            meta.lore(lores);
+        }
+        i.setItemMeta(meta);
+        return i;
     }
 }
 
